06f12e971676c54bf9007135c19e1f758488c743
Shivam Rajvanshi <srk.shivamrajvanshi@gmail.com>
2020-08-21 09:23:40 +0530

    filter is added, pagination is done

--- src/app/volunteers/volunteers-list/volunteers-list.component.ts
+++ src/app/volunteers/volunteers-list/volunteers-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatTableModule, MatTableDataSource} from '@angular/material/table';
 import { DataSource } from '@angular/cdk/table';
 // import { Observable } from 'rxjs/Observable';
 // import 'rxjs/add/observable/of';
@@ -7,9 +7,10 @@ import '@angular/material/prebuilt-themes/deeppurple-amber.css';
 import { environment } from 'src/environments/environment';
 import { MatDialog } from '@angular/material/dialog';
 import { GlobalDialogComponent } from 'src/app/global-dialog/global-dialog.component';
+import { MatSort } from '@angular/material/sort';
 // import {MatTabsModule} from '@angular/material/tabs';
 
-export interface PeriodicElement {
+export interface VolunteerDetails {
   name: string;
   position: number;
   rating: number;
@@ -21,19 +22,7 @@ export interface PeriodicElement {
   actions:any;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Surya Teja', rating: 4.5, contactNumber: 9640140999,state:'Telangana',district:'rangareddy', block:'Block1', assignedSrCitizen:20, actions:''},
-  {position: 2, name: 'James', rating: 3, contactNumber: 9640140979,state:'Maharashtra',district:'Loreimpsum', block:'Block1', assignedSrCitizen:40, actions:''},
-  {position: 3, name: 'Uma Ram', rating: 4, contactNumber: 9640140989,state:'Gujarat',district:'rangareddy', block:'Block2', assignedSrCitizen:20, actions:''},
-  {position: 4, name: 'Laxmi', rating: 3, contactNumber: 9640140969,state:'MP',district:'rangareddy', block:'Block2', assignedSrCitizen:20, actions:''},
-  {position: 5, name: 'Anderson', rating: 4, contactNumber: 9640140599,state:'Gujarat',district:'rangareddy', block:'Block1', assignedSrCitizen:20, actions:''},
-  {position: 6, name: 'Ganesh', rating: 4.5, contactNumber: 9640140949,state:'Telangana',district:'rangareddy', block:'Block1', assignedSrCitizen:20, actions:''},
-  {position: 7, name: 'Ashita', rating: 3, contactNumber: 9640140399,state:'Gujarat',district:'rangareddy', block:'Block1', assignedSrCitizen:20, actions:''},
-  {position: 8, name: 'Deen Dayal', rating: 3, contactNumber: 9640140929,state:'Maharashtra',district:'rangareddy', block:'Block1', assignedSrCitizen:20, actions:''},
-  {position: 9, name: 'Soma', rating: 4, contactNumber: 9640140919,state:'MP',district:'rangareddy', block:'Block1', assignedSrCitizen:20, actions:''},
-  {position: 10, name: 'Hakuna', rating: 4, contactNumber: 9640140909,state:'Telangana',district:'rangareddy', block:'Block1', assignedSrCitizen:20, actions:''},
-  {position: 11, name: 'Hakunam', rating: 5, contactNumber: 9640141909,state:'Telangana',district:'rangareddy', block:'Block1', assignedSrCitizen:20, actions:''},
-];
+
 
 /**
  * @title Basic use of `<mat-table>` (uses display flex)
@@ -45,17 +34,83 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./volunteers-list.component.scss']
 })
 export class VolunteersListComponent implements OnInit {
+
+  VOLUNTEER_DATA: VolunteerDetails[] = [
+    {position: 1, name: 'Surya Teja', rating: 4.5, contactNumber: 9640140999,state:'Telangana',district:'rangareddy', block:'Block1', assignedSrCitizen:20, actions:''},
+    {position: 2, name: 'James', rating: 3, contactNumber: 9640140979,state:'Maharashtra',district:'Loreimpsum', block:'Block1', assignedSrCitizen:40, actions:''},
+    {position: 3, name: 'Uma Ram', rating: 4, contactNumber: 9640140989,state:'Gujarat',district:'rangareddy', block:'Block2', assignedSrCitizen:20, actions:''},
+    {position: 4, name: 'Laxmi', rating: 3, contactNumber: 9640140969,state:'MP',district:'rangareddy', block:'Block2', assignedSrCitizen:20, actions:''},
+    {position: 5, name: 'Anderson', rating: 4, contactNumber: 9640140599,state:'Gujarat',district:'rangareddy', block:'Block1', assignedSrCitizen:20, actions:''},
+    {position: 6, name: 'Ganesh', rating: 4.5, contactNumber: 9640140949,state:'Telangana',district:'rangareddy', block:'Block1', assignedSrCitizen:20, actions:''},
+    {position: 7, name: 'Ashita', rating: 3, contactNumber: 9640140399,state:'Gujarat',district:'rangareddy', block:'Block1', assignedSrCitizen:20, actions:''},
+    {position: 8, name: 'Deen Dayal', rating: 3, contactNumber: 9640140929,state:'Maharashtra',district:'rangareddy', block:'Block1', assignedSrCitizen:20, actions:''},
+    {position: 9, name: 'Soma', rating: 4, contactNumber: 9640140919,state:'MP',district:'rangareddy', block:'Block1', assignedSrCitizen:20, actions:''},
+    {position: 10, name: 'Hakuna', rating: 4, contactNumber: 9640140909,state:'Telangana',district:'rangareddy', block:'Block1', assignedSrCitizen:20, actions:''},
+    {position: 11, name: 'Hakunam', rating: 5, contactNumber: 9640141909,state:'Telangana',district:'rangareddy', block:'Block1', assignedSrCitizen:20, actions:''},
+  ];
+
+
+
   displayedColumns: string[] = [ 'name', 'rating', 'contactNumber', 'state','district','block','assignedSrCitizen','actions'];
-  dataSource = ELEMENT_DATA;
+  dataSource = new MatTableDataSource(this.VOLUNTEER_DATA);
   links = ['Active Volunteers', 'Deboarded Volunteers'];
   activeLink = this.links[0];
   // background: ThemePalette = undefined;
 
+
+  states: string[] = [
+
+    'Andaman and Nicobar Islands', 'Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chandigarh', 'Chhattisgarh', 'Dadra and Nagar Haveli and Daman and Diu',
+
+    'Delhi', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jammu and Kashmir', 'Jharkhand', 'Karnataka', 'Kerala',
+
+    'Ladakh', 'Lakshadweep', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram',
+
+    'Nagaland', 'Odisha', 'Puducherry', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu',
+
+    'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal'
+
+  ];
+
+
+  districts: string[] = [
+
+    'Anantapur','Chittoor','East Godavari','Guntur','Kadapa','Krishna','Kurnool','Nellore','Prakasam','Srikakulam','Visakhapatnam','Vizianagaram','West Godavari',
+
+    'Anjaw','Siang','Changlang','Dibang Valley','East Kameng','East Siang','Kamle','Kra Daadi','Kurung Kumey','Lepa Rada *','Lohit','Longding','Lower Dibang Valley','Lower Siang','Lower Subansiri','Namsai','Pakke Kessang *','Papum Pare','Shi Yomi *','Tawang','Tirap','Upper Siang','Upper Subansiri','West Kameng','West Siang',
+
+  ];
+
+
+
+  blocks: string[] = [
+
+    'block1','block2','block3','Guntur','Kadapa','Krishna','Kurnool','Nellore','Prakasam','Srikakulam','Visakhapatnam','Vizianagaram','West Godavari',
+
+    'Anjaw','Siang','Changlang','Dibang Valley','East Kameng','East Siang','Kamle','Kra Daadi','Kurung Kumey','Lepa Rada *','Lohit','Longding','Lower Dibang Valley','Lower Siang','Lower Subansiri','Namsai','Pakke Kessang *','Papum Pare','Shi Yomi *','Tawang','Tirap','Upper Siang','Upper Subansiri','West Kameng','West Siang',
+
+  ];
+
+
+  sortBys: string[] = [
+
+    'Rating', 'AssignedSrCitizen',
+  
+
+  ];
+
+
+
+
+
+
+
   public base_url;
   constructor(public dialog:MatDialog) { }
-
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
   ngOnInit(): void {
     this.base_url=environment.base_url;
+    this.dataSource.sort = this.sort;
   }
   opensrCitizenAssign(){
       this.dialog.open(GlobalDialogComponent,
--- src/app/volunteers/volunteers.module.ts
+++ src/app/volunteers/volunteers.module.ts
@@ -5,6 +5,7 @@ import { VolunteersRoutingModule } from './volunteers-routing.module';
 import { VolunteersComponent } from './volunteers.component';
 import { VolunteersListComponent } from './volunteers-list/volunteers-list.component';
 import {MaterialModule} from '../material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [VolunteersComponent, VolunteersListComponent],
@@ -12,6 +13,7 @@ import {MaterialModule} from '../material/material.module';
     CommonModule,
     VolunteersRoutingModule,
     MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class VolunteersModule { }
