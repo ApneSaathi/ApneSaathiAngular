7a4fef778364c045888809f15bbe89e7c79bbca9
Shivam Rajvanshi <srk.shivamrajvanshi@gmail.com>
2020-08-26 12:44:46 +0530

    selection,fetching list using api

--- src/app/services/api-info.service.ts
+++ src/app/services/api-info.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-
+// import { VolunteerDetails} from '../volunteers/volunteers-list/volunteers-list.component';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,5 +12,13 @@ export class ApiInfoService {
   getSeniorCitizensList(): Observable<any> {
     const url = "http://15.207.42.209:8080/Volunteer/getSrCitizenList";
     return this.http.get<any>(url);
+  }
+
+
+// opost.status="Active";
+// opost.
+  postVolunteersList(opost): Observable<any> {
+    const url = "http://15.207.42.209:8080/Volunteer/getVolunteersList";
+    return this.http.post<any>(url,opost);
   }
 }
