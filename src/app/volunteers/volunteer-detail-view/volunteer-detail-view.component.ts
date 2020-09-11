c879ff2510de8055d282a3e914e80ede501f5803
Sweta Sinha <Sweta_Sinha@ad.infosys.com>
2020-09-11 19:55:43 +0530

    commit for add sr citizen and assigned sr citizen list in volunteer details page

--- src/app/volunteers/volunteer-detail-view/volunteer-detail-view.component.ts
+++ src/app/volunteers/volunteer-detail-view/volunteer-detail-view.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import {ApiInfoService} from 'src/app/services/api-info.service';
+import { GlobalDialogComponent } from 'src/app/global-dialog/global-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-volunteer-detail-view',
@@ -12,26 +14,53 @@ export class VolunteerDetailViewComponent implements OnInit {
 
   public volunteerId;
   public base_url;
-  volunteerDetailsDataSource;
+  public volunteerDetailsDataSource;
+  public assignedCitizensDataSource: object[];
 
-  constructor(private route: ActivatedRoute, private router: Router, private apiInfoService: ApiInfoService) { }
+  constructor(private route: ActivatedRoute, private router: Router, private apiInfoService: ApiInfoService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
 
     this.base_url=environment.base_url;
-
     this.route.paramMap.subscribe((params: ParamMap) => {
       let id = parseInt(params.get('id'));
       this.volunteerId = id;
     });
+    this.fetchDetails();
 
+  }
+
+  fetchDetails() {
     this.apiInfoService.getVolunteerDetails({id: this.volunteerId}).subscribe((data) => {
-      this.volunteerDetailsDataSource = data.volunteer;
+      this.volunteerDetailsDataSource = data.volunteerVO;
+      this.assignedCitizensDataSource = data.volunteerVO.srCitizenList;
     })
-    
   }
 
   gotoVolunteerList() {
     this.router.navigate(['volunteers/voluntersList',{id: this.volunteerId}]);
   }
+
+  openGlobalPopup(configurationObject){
+    const dialogRef = this.dialog.open(GlobalDialogComponent,configurationObject);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.fetchDetails();
+    });
+  }
+
+  opensrCitizenAssign(volunteer){
+    let configObject ={
+      data:{
+        heading:"Assign Sr.citizens",
+        feature: "assignCitizensSingleVolunteer",
+        volunteerObj: volunteer
+      },
+      disableClose:true,
+      width: "70%",
+      autoFocus: false,
+    };
+    this.openGlobalPopup(configObject);
+  }
+  
 }
